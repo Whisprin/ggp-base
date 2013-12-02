8c7184cc185e84b98e6b7e0946fdfba9a9c30443
Robert Sprunk <sprunk@informatik.hu-berlin.de>
2013-12-02 11:37:37 +0100

    added our own Player

---
+++ src/org/ggp/base/player/gamer/statemachine/hu/Tromboter.java
@@ -0,0 +1,108 @@
+package org.ggp.base.player.gamer.statemachine.hu;
+
+import java.util.List;
+
+import org.ggp.base.apps.player.detail.DetailPanel;
+import org.ggp.base.apps.player.detail.SimpleDetailPanel;
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.exception.GamePreviewException;
+import org.ggp.base.player.gamer.statemachine.StateMachineGamer;
+import org.ggp.base.util.game.Game;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.StateMachine;
+import org.ggp.base.util.statemachine.cache.CachedStateMachine;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+import org.ggp.base.util.statemachine.implementation.prover.ProverStateMachine;
+
+/**
+ * SampleLegalGamer is a minimal gamer which always plays the first
+ * legal move it identifies, regardless of the state of the game.
+ *
+ * For your first players, you should extend the class SampleGamer
+ * The only function that you are required to override is :
+ * public Move stateMachineSelectMove(long timeout)
+ *
+ */
+public final class Tromboter extends StateMachineGamer
+{
+	/**
+	 * This function is called at the start of each round
+	 * You are required to return the Move your player will play
+	 * before the timeout.
+	 *
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		// We get the current start time
+		long start = System.currentTimeMillis();
+
+		/**
+		 * We put in memory the list of legal moves from the
+		 * current state. The goal of every stateMachineSelectMove()
+		 * is to return one of these moves. The choice of which
+		 * Move to play is the goal of GGP.
+		 */
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+
+		// SampleLegalGamer is very simple : it picks the first legal move
+		Move selection = moves.get(0);
+
+		// We get the end time
+		// It is mandatory that stop<timeout
+		long stop = System.currentTimeMillis();
+
+		/**
+		 * These are functions used by other parts of the GGP codebase
+		 * You shouldn't worry about them, just make sure that you have
+		 * moves, selection, stop and start defined in the same way as
+		 * this example, and copy-paste these two lines in your player
+		 */
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+
+	@Override
+	public StateMachine getInitialStateMachine() {
+		return new CachedStateMachine(new ProverStateMachine());
+	}
+
+	@Override
+	public void stateMachineMetaGame(long timeout)
+			throws TransitionDefinitionException, MoveDefinitionException,
+			GoalDefinitionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineStop() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stateMachineAbort() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preview(Game g, long timeout) throws GamePreviewException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getName() {
+		return "Tromboter";
+	}
+
+	// This is the defaul Sample Panel
+	@Override
+	public DetailPanel getDetailPanel() {
+		return new SimpleDetailPanel();
+	}
+}
